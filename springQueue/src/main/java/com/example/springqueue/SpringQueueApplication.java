6fa5ac0c953bf9a9e8ac284da7b975473bcffc69
Herivelton Coelho <hericoelho@gmail.com>
2026-07-26 14:47:09 -0300

    chore: spring core

---
+++ springQueue/src/main/java/com/example/springqueue/SpringQueueApplication.java
@@ -0,0 +1,13 @@
+package com.example.springqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringQueueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringQueueApplication.class, args);
+    }
+
+}
