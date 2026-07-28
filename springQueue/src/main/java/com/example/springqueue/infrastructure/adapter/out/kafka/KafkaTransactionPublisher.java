8933fcdb2d84cfd72139fa47b5d7045e2e530093
Herivelton Coelho <hericoelho@gmail.com>
2026-07-27 21:00:00 -0300

    feat: add circuite break and tracing

--- springQueue/src/main/java/com/example/springqueue/infrastructure/adapter/out/kafka/KafkaTransactionPublisher.java
+++ springQueue/src/main/java/com/example/springqueue/infrastructure/adapter/out/kafka/KafkaTransactionPublisher.java
@@ -2,6 +2,8 @@ package com.example.springqueue.infrastructure.adapter.out.kafka;
 
 import com.example.springqueue.application.ports.out.PublishTransactionPort;
 import com.example.springqueue.domain.model.TransactionEvent;
+import com.example.springqueue.domain.model.exception.BusinessException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,8 +20,15 @@ public class KafkaTransactionPublisher implements PublishTransactionPort {
     private String transactionTopic;
 
     @Override
+    @CircuitBreaker(name = "kafkaPublisher", fallbackMethod = "publishFallback")
     public void publish(TransactionEvent transactionEvent) {
         kafkaTemplate.send(transactionTopic, transactionEvent.id(), transactionEvent);
         log.info("Sent to Kafka: topic={}, key={}", transactionTopic, transactionEvent.id());
+    }
+
+    public void publishFallback(TransactionEvent transactionEvent, Exception ex) {
+        log.error("Circuit breaker fallback: unable to publish transaction id={}. Error: {}",
+                transactionEvent.id(), ex.getMessage());
+        throw new BusinessException("Kafka unavailable: " + ex.getMessage());
     }
 }
